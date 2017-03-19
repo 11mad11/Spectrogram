a005cfa3d1219756937f540766ed31a1b4ed9c8a
21mad21 <mad.21@live.fr>
2017-03-18 20:07:10 -0400

    Original

---
+++ html/src/fr/mad/spectrogram/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package fr.mad.spectrogram.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import fr.mad.spectrogram.Core;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Core();
+        }
+}

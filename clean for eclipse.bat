f4592b02990f63444687a69f309e3190cae00c71
21mad21 <mad.21@live.fr>
2017-03-20 18:30:15 -0400

    updated tool clean for eclipse to take android sdk path

--- clean for eclipse.bat
+++ clean for eclipse.bat
@@ -1,2 +1,12 @@
+@echo off
+set /p url="Enter Android sdk path: "
+if "%url%"=="" goto clean
+pause
+set NEW_PATH=%url:\=/%
+echo # Location of the android SDK>local.properties
+echo sdk.dir=%NEW_PATH%>>local.properties
+:clean
 call gradlew cleanEclipse
 call gradlew eclipse
+echo end
+exit
